74a48ceec10ccc96086a3f247d5175166e4dce63
Eli Orians <eli.orians@gmail.com>
2023-10-03 18:38:14 -0400

    got kendricks albums into a csv file

--- SpotifyVisualizer.py
+++ SpotifyVisualizer.py
@@ -2,14 +2,35 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
-load_dotenv('./.env')
+#load secret credentials
+load_dotenv("./.env")
 ClientID=os.getenv("ClientID")
 Clientsecret=os.getenv("Clientsecret")
 
 
+#artist IDs
+artist_id= {
+    "kendrickLamar": "2YZyLoL8N0Wb9xBt1NhZWg",
+    "zachBryan": "40ZNYROS4zLfyyBSs2PGe2",
+    "taylorSwift": "06HL4z0CvFAxyc27GXpf02",
+    "lilKee" : "21UqznWenhsInMOKxpVPBd"
+}
+
 def main():
-    print('hello')
+
+    #Authentication (without user)
+    client_credentials_manager = SpotifyClientCredentials(client_id=ClientID, client_secret=Clientsecret)
+    spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    
+    #select the artist
+    artist = artist_id["kendrickLamar"]
+
+    #artist album data into dataframe
+    artistAlbums = pd.DataFrame(spotify.artist_albums(artist))
+    #dataframe to csv
+    artistAlbums.to_csv('./output.csv')
 
 if __name__ == "__main__":
     main()
